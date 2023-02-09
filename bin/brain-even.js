3c3697ed5c5c9e55c898dce3e4c340b44c43a2b2
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-02-09 14:45:26 +0500

    modified brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -8,10 +8,10 @@ const name = readlineSync.question('Your name:');
 console.log(`Hello, ${name}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 let counter = 0;
-while (counter < 3) {
-    let number = Math.floor(Math.random() * 50);
+let number = Math.floor(Math.random() * 50);
     console.log(`Question: ${number}`);
     const answer = readlineSync.question('Your answer:');
+while (counter < 3) {
     if(answer !== 'yes' && answer !== 'no') {
       console.log('Please, use yes or no for answer!');
     }
@@ -19,13 +19,21 @@ while (counter < 3) {
       console.log('Correct!');
       counter += 1;
     } else if (answer === 'yes') {
-        console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
+        break;
     }
     if (number % 2 !== 0 && answer === 'no') {
         console.log('Correct!');
         counter += 1; 
     } else if (number % 2 === 0 && answer === 'no') {
-        console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
+      break;
     }
 }
-console.log(`Congratulations, ${name}!`);
+if (counter === 3) {
+  console.log(`Congratulations, ${name}!`);
+}
+if (answer === 'yes') {
+  console.log(`'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again,${name}!`);
+} else {
+  console.log(`'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again,${name}!`);
+}
+
