e330053583ec7725734d32699db9697e0d3a4cee
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-02-10 02:37:38 +0500

    update brain-calc.js

---
+++ bin/games/brain-calc.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+const name = readlineSync.question('Your name:');
+console.log(`Hello, ${name}!`);
+console.log('What is the result of the expression?');
+let counter = 0;
+let resultOfExpression = 0;
+let expression = '';
+const arrayOfSym = ['+', '-', '*'];
+while (counter < 3) {
+    let firstNumber = Math.floor(Math.random() * 50);
+    let secondNumber = Math.floor(Math.random() * 50);
+    let indexOfArray = Math.floor(Math.random() * 3);
+    let symbol = arrayOfSym[indexOfArray];
+    if (symbol === '+') {
+        resultOfExpression = firstNumber + secondNumber;
+        expression = `${firstNumber} + ${secondNumber}`;
+        console.log(`Question: ${expression}`);
+    }
+    if (symbol === '-') {
+        resultOfExpression = firstNumber - secondNumber;
+        expression = `${firstNumber} - ${secondNumber}`;
+        console.log(`Question: ${expression}`);
+    }
+    if (symbol === '*') {
+        resultOfExpression = firstNumber * secondNumber;
+        expression = `${firstNumber} * ${secondNumber}`;
+        console.log(`Question: ${expression}`);
+    }
+    const answer = Number(readlineSync.question('Your answer:'));
+    if (answer === resultOfExpression) {
+            counter += 1;
+            console.log('Correct!');
+    }
+    if (answer !== resultOfExpression) {
+    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${resultOfExpression}'\nLet's try again,${name}!`);
+    break;
+    }
+}
+if (counter === 3) {
+    console.log(`Congratulations, ${name}!`);
+}
