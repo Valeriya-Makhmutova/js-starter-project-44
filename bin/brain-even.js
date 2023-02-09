2166f7fcd6235670d2f14ed35c867360641ac8a3
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-02-09 13:02:05 +0500

    update package.jsone, btain-even.js

---
+++ bin/brain-even.js
@@ -0,0 +1,27 @@
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+console.log('May I have your name?');
+const name = readlineSync.question('Your name:');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+let counter = 0;
+while (counter < 3) {
+    let number = Math.floor(Math.random() * 50);
+    console.log(`Question: ${number}`);
+    const answer = readlineSync.question('Your answer:');
+    if (number % 2 === 0 && answer === 'yes') {
+      console.log('Correct');
+      counter += 1;
+    } else if (answer === 'yes') {
+        console.log("'yes' is wrong answer ;(. Correct answer was 'no'");
+    }
+    if (number % 2 !== 0 && answer === 'no') {
+        console.log('Correct!');
+        counter += 1; 
+    } else if (number % 2 === 0 && answer === 'no') {
+        console.log("'no' is wrong answer ;(. Correct answer was 'yes'");
+    }
+}
+console.log(`Congratulations, ${name}!`);
