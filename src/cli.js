0830f55fa3bebe631dd4d806d412a955407f58b3
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-02-11 23:16:08 +0500

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -3,4 +3,5 @@ import readlineSync from 'readline-sync';
 export const askNameAndGreet = () => {
   const name = readlineSync.question('Your name:');
   console.log(`Hello, ${name}!`);
+  return name;
 };
