482d18a28f8d8d22da7fdf2147b8017a66008764
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-02-12 11:04:21 +0500

    fix bug in brain-even

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -20,14 +20,14 @@ while (counter < 3) {
       console.log('Correct!');
       counter += 1;
     } else if (answer === 'yes') {
-        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again,${name}!`);
+        console.log(`'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again, ${name}!`);
         break;
     }
   if (number % 2 !== 0 && answer === 'no') {
         console.log('Correct!');
         counter += 1; 
     } else if (number % 2 === 0 && answer === 'no') {
-      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again,${name}!`);
+      console.log(`'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, ${name}!`);
       break;
     }
 }
