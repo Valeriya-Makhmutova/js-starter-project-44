efa5503e9440dfe1682f2eb9d959bbe932bde366
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-05-30 17:43:41 +0500

    updated brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,29 +1,52 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { getGeneralDivider } from '../../src/utils.js';
-import askNameAndGreet from '../../src/cli.js';
+import { sayHi, askNameAndGreet } from '../cli.js';
+import { playGame } from '../playGame.js';
+import { getGeneralDivider } from '../utils.js';
 
-console.log('Welcome to the Brain Games!');
-console.log('May I have your name?');
-const name = askNameAndGreet();
-console.log('Find the greatest common divisor of given numbers.');
-let i = 0;
-while (i < 3) {
-  const firstNum = Math.floor(Math.random() * 100);
-  const secondNum = Math.floor(Math.random() * 100);
-  console.log(`Question: ${firstNum} ${secondNum}`);
+const brainGcd = () => {
+  sayHi();
+  const name = askNameAndGreet();
+  const gameConfig = [];
 
-  const result = getGeneralDivider(firstNum, secondNum);
-  const answer = Number(readlineSync.question('Your answer:'));
-  if (answer === result) {
-    i += 1;
-    console.log('Correct!');
+  let counter = 0;
+  while (counter < 3) {
+    const firstNum = Math.floor(Math.random() * 100);
+    const secondNum = Math.floor(Math.random() * 100);
+    const expression = `${firstNum} ${secondNum}`;
+    const answer = getGeneralDivider(firstNum, secondNum);
+    gameConfig.push([expression, answer])
+    counter = counter + 1;
   }
-  if (answer !== result) {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'\nLet's try again, ${name}!`);
-    break;
-  }
-}
-if (i === 3) {
-  console.log(`Congratulations, ${name}!`);
-}
+  console.log('Find the greatest common divisor of given numbers.');
+  playGame(gameConfig, name);
+};
+
+export default brainGcd;
+
+
+
+
+
+
+// console.log('Find the greatest common divisor of given numbers.');
+// let i = 0;
+// while (i < 3) {
+//   const firstNum = Math.floor(Math.random() * 100);
+//   const secondNum = Math.floor(Math.random() * 100);
+  
+//   console.log(`Question: ${firstNum} ${secondNum}`);
+
+//   const result = getGeneralDivider(firstNum, secondNum);
+//   const answer = Number(readlineSync.question('Your answer:'));
+//   if (answer === result) {
+//     i += 1;
+//     console.log('Correct!');
+//   }
+//   if (answer !== result) {
+//     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${result}'\nLet's try again, ${name}!`);
+//     break;
+//   }
+// }
+// if (i === 3) {
+//   console.log(`Congratulations, ${name}!`);
+// }
