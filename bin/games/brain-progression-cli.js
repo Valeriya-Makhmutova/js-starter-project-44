5b065b2a31bf80e86ae26b4593cb8634495f7e8a
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-05-30 18:59:30 +0500

    updated brain-progression

--- bin/games/brain-progression-cli.js
+++ bin/games/brain-progression-cli.js
@@ -1,36 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import askNameAndGreet from '../../src/cli.js';
+import brainProgression from '../../src/games/brain-progression.js';
 
-console.log('Welcome to the Brain Games!');
-console.log('May I have your name?');
-const name = askNameAndGreet();
-console.log('What number is missing in the progression?');
-let step = 0;
-while (step < 3) {
-  const array = [];
-  let randomNumber = Math.floor(Math.random() * 20);
-  const counter = Math.floor(Math.random() * 10);
-  let index = 0;
-  while (index < 10) {
-    array.push(randomNumber);
-    randomNumber += counter;
-    index += 1;
-  }
-  const i = Math.floor(Math.random() * 9);
-  const checkNumber = array[i];
-  array[i] = '..';
-  console.log(`Question: ${array.join(' ')}`);
-  const answer = Number(readlineSync.question('Your answer:'));
-  if (answer === checkNumber) {
-    console.log('Correct!');
-    step += 1;
-  }
-  if (answer !== checkNumber) {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkNumber}'\nLet's try again, ${name}!`);
-    break;
-  }
-}
-if (step === 3) {
-  console.log(`Congratulations, ${name}!`);
-}
+brainProgression();
--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,36 +1,65 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import askNameAndGreet from '../../src/cli.js';
+import { sayHi, askNameAndGreet } from '../cli.js';
+import { playGame } from '../playGame.js';
 
-console.log('Welcome to the Brain Games!');
-console.log('May I have your name?');
-const name = askNameAndGreet();
-console.log('What number is missing in the progression?');
-let step = 0;
-while (step < 3) {
-  const array = [];
-  let randomNumber = Math.floor(Math.random() * 20);
-  const counter = Math.floor(Math.random() * 10);
-  let index = 0;
-  while (index < 10) {
+
+const brainProgression = () => {
+  sayHi();
+  const name = askNameAndGreet();
+  const gameConfig = [];
+
+  let counter = 0;
+  while (counter < 3) {
+    const array = [];
+    let randomNumber = Math.floor(Math.random() * 20);
+    const different = Math.floor(Math.random() * 10);
+    let index = 0;
+    while (index < 10) {
     array.push(randomNumber);
-    randomNumber += counter;
+    randomNumber += different;
     index += 1;
+    }
+    const i = Math.floor(Math.random() * 9);
+    const checkNumber = array[i];
+    array[i] = '..';
+
+    const expression = `${array.join(' ')}`;
+    const answer = checkNumber;
+    gameConfig.push([expression, answer])
+    counter = counter + 1;
   }
-  const i = Math.floor(Math.random() * 9);
-  const checkNumber = array[i];
-  array[i] = '..';
-  console.log(`Question: ${array.join(' ')}`);
-  const answer = Number(readlineSync.question('Your answer:'));
-  if (answer === checkNumber) {
-    console.log('Correct!');
-    step += 1;
-  }
-  if (answer !== checkNumber) {
-    console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkNumber}'\nLet's try again, ${name}!`);
-    break;
-  }
-}
-if (step === 3) {
-  console.log(`Congratulations, ${name}!`);
-}
+  console.log('What number is missing in the progression?');
+  playGame(gameConfig, name);
+};
+
+export default brainProgression;
+
+
+// let step = 0;
+// while (step < 3) {
+//   const array = [];
+//   let randomNumber = Math.floor(Math.random() * 20);
+//   const counter = Math.floor(Math.random() * 10);
+//   let index = 0;
+//   while (index < 10) {
+//     array.push(randomNumber);
+//     randomNumber += counter;
+//     index += 1;
+//   }
+//   const i = Math.floor(Math.random() * 9);
+//   const checkNumber = array[i];
+//   array[i] = '..';
+//   console.log(`Question: ${array.join(' ')}`);
+//   const answer = Number(readlineSync.question('Your answer:'));
+//   if (answer === checkNumber) {
+//     console.log('Correct!');
+//     step += 1;
+//   }
+//   if (answer !== checkNumber) {
+//     console.log(`'${answer}' is wrong answer ;(. Correct answer was '${checkNumber}'\nLet's try again, ${name}!`);
+//     break;
+//   }
+// }
+// if (step === 3) {
+//   console.log(`Congratulations, ${name}!`);
+// }
