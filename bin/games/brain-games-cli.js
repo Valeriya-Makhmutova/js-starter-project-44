86c4540ea0b927776547a174b29c517bd33a3a89
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-05-30 19:19:07 +0500

    deleted line in brain-games-cli.js

--- bin/games/brain-games-cli.js
+++ bin/games/brain-games-cli.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-import askNameAndGreet from '../../src/cli.js';
+import { askNameAndGreet } from '../../src/cli.js';
 
 console.log('brain-games');
 console.log('Welcome to the Brain Games!');
-console.log('May I have your name?');
 askNameAndGreet();
