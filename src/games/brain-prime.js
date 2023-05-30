a7339ceec8e27b037b9f055a3dc946ed145999ec
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-05-30 18:15:56 +0500

    updated brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,46 +1,41 @@
 #!/usr/bin/env node
+import { sayHi, askNameAndGreet } from '../cli.js';
+import { playGame } from '../playGame.js';
 
-import readlineSync from 'readline-sync';
-import askNameAndGreet from '../../src/cli.js';
-
-console.log('Welcome to the Brain Games!');
-console.log('May I have your name?');
-const name = askNameAndGreet();
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-const arrayWithNumbers = [];
-let i = 0;
-while (i < 50) {
-  arrayWithNumbers.push(i);
-  i += 1;
-}
-let counter = 0;
-while (counter < 3) {
-  const arrayForCheck = [];
-  const number = Math.floor(Math.random() * 50);
-  console.log(`Question: ${number}`);
-  const answer = readlineSync.question('Your answer:');
-  for (let index = 0; index < arrayWithNumbers.length; index += 1) {
-    if (number % arrayWithNumbers[index] === 0) {
-      arrayForCheck.push(arrayWithNumbers[index]);
+const brainPrime = () => {
+  sayHi();
+  const name = askNameAndGreet();
+  const gameConfig = [];
+  const arrayWithNumbers = [];
+  let i = 0;
+  while (i < 50) {
+    arrayWithNumbers.push(i);
+    i += 1;
+  }
+  let counter = 0;
+  while (counter < 3) {
+    const arrayForCheck = [];
+    const expression = Math.floor(Math.random() * 50);
+    for (let index = 0; index < arrayWithNumbers.length; index += 1) {
+      if (expression % arrayWithNumbers[index] === 0) {
+        arrayForCheck.push(arrayWithNumbers[index]);
+      }
     }
+    if (arrayForCheck.length > 3) {
+      const answer = 'no';
+      gameConfig.push([expression, answer]);
+      counter = counter + 1;
+    }
+    if (arrayForCheck.length < 3) {
+      const answer = 'yes';
+      gameConfig.push([expression, answer]);
+      counter = counter + 1;
+    }
+
   }
-  if (arrayForCheck.length < 3 && answer === 'yes') {
-    console.log('Correct!');
-    counter += 1;
-  }
-  if (arrayForCheck.length < 3 && answer === 'no') {
-    console.log(`'no' is wrong answer ;(. Correct answer was 'yes'\nLet's try again, ${name}!`);
-    break;
-  }
-  if (arrayForCheck.length >= 3 && answer === 'yes') {
-    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'\nLet's try again, ${name}!`);
-    break;
-  }
-  if (arrayForCheck.length >= 3 && answer === 'no') {
-    console.log('Correct!');
-    counter += 1;
-  }
-}
-if (counter === 3) {
-  console.log(`Congratulations, ${name}!`);
-}
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  playGame(gameConfig, name);
+};
+
+export default brainPrime;
+
