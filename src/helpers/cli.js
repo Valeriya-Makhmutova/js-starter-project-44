a379f8c7bfeb8e6e1a02f142e8b80868c0de7aef
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-06-09 18:16:01 +0500

    moved into directory /helpers

---
+++ src/helpers/cli.js
@@ -0,0 +1,12 @@
+import readlineSync from 'readline-sync';
+
+export const sayHi = () => {
+  console.log('Welcome to the Brain Games!');
+};
+
+export const askNameAndGreet = () => {
+  console.log('May I have your name?');
+  const name = readlineSync.question('Your name:');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
