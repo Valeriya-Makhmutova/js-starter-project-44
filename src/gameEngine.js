1aa486a7ed0f8fc355e8139a6a9f3b1e9737e733
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-06-09 18:17:39 +0500

    renamed playGames into gameEngine and changed itt

---
+++ src/gameEngine.js
@@ -0,0 +1,36 @@
+import { askNameAndGreet, sayHi } from "./helpers/cli.js";
+import readlineSync from 'readline-sync';
+
+
+const gameEngine = (gameRules, gameFunction) => {
+  let name = '';
+  let counter = 0;
+  const [task, repeats, greeting] = gameRules;
+  if (greeting) {
+    sayHi();
+    name = askNameAndGreet();
+  }
+  console.log(task);
+  while (counter < repeats) {
+    const [question, answer] = gameFunction();
+    console.log(`Question: ${question}`);
+    let userAnswer = null;
+    if (typeof answer === 'number') {
+      userAnswer = Number(readlineSync.question('Your answer:'));
+    } else {
+      userAnswer = readlineSync.question('Your answer:');
+    }
+
+    if (answer === userAnswer) {
+      counter += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+  if (counter === repeats) console.log(`Congratulations, ${name}!`);
+};
+
+
+export default gameEngine;
