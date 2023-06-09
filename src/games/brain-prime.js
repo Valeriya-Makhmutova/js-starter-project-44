7fa07b084cf26d4023abac6a044364be5e63560c
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-06-09 18:56:00 +0500

    fixed structure of brain-prime.js

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,45 +1,43 @@
 #!/usr/bin/env node
-import { sayHi, askNameAndGreet } from '../cli.js';
-import playGame from '../playGame.js';
-import generateNumber from '../number-generator.js';
+import gameEngine from '../gameEngine.js';
+import generateNumber from '../helpers/number-generator.js';
 
 const startPrimeOrNotNumberGame = () => {
-  sayHi();
-  const name = askNameAndGreet();
-  const gameConfig = [];
-  const arrayWithNumbers = [];
-  const minNumberOfQuestions = 3;
-  const numberOfDigits = 50;
+  const repeats = 3;
+  const greeting = true;
+  const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+  const gameRules = [task, repeats, greeting];
 
-  let i = 0;
-  while (i < numberOfDigits) {
-    arrayWithNumbers.push(i);
-    i += 1;
-  }
-  let counter = 0;
-  while (counter < minNumberOfQuestions) {
+
+  const getBrainPrime = () => {
+    let answer = '';
+    const arrayWithNumbers = [];
+    const numberOfDigits = 50;
+    let i = 0;
+    while (i < numberOfDigits) {
+      arrayWithNumbers.push(i);
+      i += 1;
+    }
+
     const maxDividersForPrimeNum = 3;
     const arrayForCheck = [];
-    const expression = generateNumber(1, 50);
-    
+    const question = generateNumber(1, 50);
+
     for (let index = 0; index < arrayWithNumbers.length; index += 1) {
-      if (expression % arrayWithNumbers[index] === 0) {
+      if (question % arrayWithNumbers[index] === 0) {
         arrayForCheck.push(arrayWithNumbers[index]);
       }
     }
     if (arrayForCheck.length > maxDividersForPrimeNum) {
-      const answer = 'no';
-      gameConfig.push([expression, answer]);
-      counter += 1;
+      answer = 'no';
     }
     if (arrayForCheck.length < maxDividersForPrimeNum) {
-      const answer = 'yes';
-      gameConfig.push([expression, answer]);
-      counter += 1;
+      answer = 'yes';
     }
-  }
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  playGame(gameConfig, name);
+    const result = [question, answer];
+    return result;
+  };
+  gameEngine(gameRules, getBrainPrime);
 };
 
 export default startPrimeOrNotNumberGame;
