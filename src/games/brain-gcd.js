c0c84f37c183e5435843134a39e3ecaa1cc480c2
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-06-09 18:41:48 +0500

    fixed structure of brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,26 +1,23 @@
 #!/usr/bin/env node
-import { sayHi, askNameAndGreet } from '../cli.js';
-import playGame from '../playGame.js';
-import { getGeneralDivider } from '../utils.js';
-import generateNumber from '../number-generator.js';
+import gameEngine from '../gameEngine.js';
+import { getGeneralDivider } from '../helpers/utils.js';
+import generateNumber from '../helpers/number-generator.js';
 
 const startGreatestCommonDivisorGame = () => {
-  sayHi();
-  const name = askNameAndGreet();
-  const gameConfig = [];
-  const minNumberOfQuestions = 3;
+  const repeats = 3;
+  const greeting = true;
+  const task = 'Find the greatest common divisor of given numbers.';
+  const gameRules = [task, repeats, greeting];
 
-  let counter = 0;
-  while (counter < minNumberOfQuestions) {
+  const getBrainGcd = () => {
     const firstNum = generateNumber(1, 100);
     const secondNum = generateNumber(1, 100);
-    const expression = `${firstNum} ${secondNum}`;
+    const question = `${firstNum} ${secondNum}`;
     const answer = getGeneralDivider(firstNum, secondNum);
-    gameConfig.push([expression, answer]);
-    counter += 1;
-  }
-  console.log('Find the greatest common divisor of given numbers.');
-  playGame(gameConfig, name);
+    const result = [question, answer];
+    return result;
+  };
+  gameEngine(gameRules, getBrainGcd);
 };
 
 export default startGreatestCommonDivisorGame;
