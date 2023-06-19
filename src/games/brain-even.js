83addcdd456df5eeae80d2c6d818db139922a5d3
Valeriya Makhmutova <mavapa369.yandex.ru>
2023-06-19 16:08:04 +0500

    added function isEvenNumber in brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,19 +2,24 @@
 import gameEngine from '../gameEngine.js';
 import generateNumber from '../helpers/number-generator.js';
 
+const isEvenNumber = (number) => {
+  if (number % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 const startEvenOrNotGame = () => {
   const task = 'Answer "yes" if the number is even, otherwise answer "no".';
   const gameRules = [task];
 
   const getBrainEven = () => {
-    const question = generateNumber(1, 50);
     let answer = '';
-    if (question % 2 === 0) {
-      answer = 'yes';
-    }
-    if (question % 2 !== 0) {
-      answer = 'no';
-    }
+    const question = generateNumber(1, 50);
+
+    isEvenNumber(question) ? answer = 'yes' : answer = 'no';
+    
     const result = [question, answer];
     return result;
   };
