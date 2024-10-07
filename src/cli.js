56e3cd3f648f945cdbf90832eb004b05924dbe2e
Valeriya-Makhmutova <mavapa369@yandex.ru>
2024-10-07 23:24:48 +0500

    added user greeting with name (step 3)

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export const askUserName = () => {
+  console.log('May I have your name?');
+  const name = readlineSync.question('My name is: ');
+  return name;
+}
